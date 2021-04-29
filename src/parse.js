9116348cb8a37d8790bb47be5d93cd06eb57d4ad
Cafe137 <aron@aronsoos.com>
2021-04-29 11:32:28 +0200

    rename argument to rawValue in parseValue

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,11 @@
-function parseValue(option, argument) {
+function parseValue(option, rawValue) {
     const { key, type } = option
     if (type === 'boolean') {
         return { value: true, skip: 0 }
     } else if (type === 'number') {
-        const value = parseInt(argument, 10)
+        const value = parseInt(rawValue, 10)
         if (isNaN(value)) {
-            return Error('Expected number for ' + key + ', got ' + argument)
+            return Error('Expected number for ' + key + ', got ' + rawValue)
         }
         if (option.minimum !== undefined && value < option.minimum) {
             return Error('[' + key + '] must be at least ' + option.minimum)
@@ -13,16 +13,16 @@ function parseValue(option, argument) {
         return { value, skip: 1 }
     } else if (type === 'bigint') {
         try {
-            const value = BigInt(argument)
+            const value = BigInt(rawValue)
             if (option.minimum !== undefined && value < option.minimum) {
                 return Error('[' + key + '] must be at least ' + option.minimum)
             }
             return { value, skip: 1 }
         } catch {
-            return Error('Expected BigInt for ' + key + ', got ' + argument)
+            return Error('Expected BigInt for ' + key + ', got ' + rawValue)
         }
     } else {
-        return { value: argument, skip: 1 }
+        return { value: rawValue, skip: 1 }
     }
 }
 
